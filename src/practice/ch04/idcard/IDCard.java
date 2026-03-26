03c1e48e41327ea4a7613855b561eead1e40f6d3
hanleeann <hanleeann@duksung.ac.kr>
2026-03-26 11:13:41 +0900

    dd

---
+++ src/practice/ch04/idcard/IDCard.java
@@ -0,0 +1,27 @@
+package practice.ch04.idcard;
+
+import practice.ch04.framework.Product;
+
+public class IDCard extends Product {
+    private String owner;
+
+    IDCard(String owner) {
+        System.out.println(owner + "의 카드를 만듭니다.");
+        this.owner = owner;
+    }
+
+    @Override
+    public void use() {
+        System.out.println(this.toString() + "을 사용합니다.");
+    }
+
+    @Override
+    // Object 클래스의 toString() 메소드 오버라이드
+    public String toString() {
+        return "[IDCard:" + owner + "]";
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}
